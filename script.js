c6fed549636dd09e20c620ee23d9eb3030ab56fe
BrunoL9 <juegos.malafar@gmail.com>
2022-05-05 02:12:24 -0300

    Se crean HTML CSS  y Javascript de la calculadora
    Queda pendiente hacer que se adapte correctamente a distintas resoluciones.

---
+++ script.js
@@ -0,0 +1,115 @@
+// Variables -------------------------------------------------------------------------------------------------------
+
+const botonesNumeros = document.querySelectorAll('[data-numero]');
+const botonesOperadores = document.querySelectorAll('[data-operador]');
+const botonCe = document.querySelector('[data-borrar-todo]'); // borra todo
+const botonC = document.querySelector('[data-borrar]'); // borra ultima entrada
+const botonIgual = document.querySelector('[data-igual]'); //ejecuta la operacion cargada
+const operacionAnterior = document.querySelector('[data-operacion-arriba]'); // parte superior del display
+const operacionActual = document.querySelector('[data-operacion-abajo]'); // parte inferior del display
+let operacion;
+let resultado;
+
+// Funciones -------------------------------------------------------------------------------------------------------
+
+function escribirNumero(botonNumero) {
+  if (operacionActual.innerText === "0") {
+    operacionActual.innerText = "";
+  }
+
+  if (botonNumero.innerText === "." && operacionActual.innerText.includes(".")) return;
+
+  operacionActual.innerText += botonNumero.innerText;
+
+  if (operacionActual.innerText === ".") {
+    operacionActual.innerText = "0.";
+  }
+}
+
+function elegirOperacion(botonesOperadores) {
+  if (operacionActual.innerText === "") return;
+  if (operacionActual.innerText.slice(-1) === ".") {
+    operacionActual.innerText = operacionActual.innerText.slice(0, -1);
+  }
+
+  operacion = botonesOperadores.innerText;
+
+  if (operacionActual.innerText.includes(".")) {
+    operacionActual.innerText = parseFloat(operacionActual.innerText).toFixed(5);
+  }
+
+  operacionAnterior.innerText = `${operacionActual.innerText} ${operacion}`;
+  operacionActual.innerText = "";
+}
+
+function calcular() {
+
+  if (operacionActual.innerText === "" || operacionAnterior.innerText === "") return;
+
+  const numeroAnterior = parseFloat(operacionAnterior.innerText);
+  const numeroActual = parseFloat(operacionActual.innerText);
+
+  switch (operacion) {
+    case "+":
+      resultado = numeroAnterior + numeroActual;
+      break;
+    case "-":
+      resultado = numeroAnterior - numeroActual;
+      break;
+    case "x":
+      resultado = numeroAnterior * numeroActual;
+      break;
+    case "/":
+      resultado = numeroAnterior / numeroActual;
+      break;
+  }
+
+  operacionAnterior.innerText = "";
+
+  if (Number.isInteger(resultado) === false) {
+    resultado = resultado.toFixed(5);
+  }
+
+  operacionActual.innerText = resultado;
+}
+
+function borrarTodo() {
+  operacionAnterior.innerText = "";
+  operacionActual.innerText = "0";
+  resultado = null;
+}
+
+function borrar() {
+  if (operacionActual.innerText === "0") return;
+  if (operacionActual.innerText.length === 1) {
+    operacionActual.innerText = "0";
+  } else {
+    operacionActual.innerText = operacionActual.innerText.slice(0, -1);
+  }
+}
+
+// Eventos  -------------------------------------------------------------------------------------------------------
+
+botonesNumeros.forEach(boton => {
+  boton.addEventListener('click', () => {
+    escribirNumero(boton);
+  });
+});
+
+botonesOperadores.forEach(boton => {
+  boton.addEventListener('click', () => {
+    elegirOperacion(boton);
+  });
+});
+
+botonCe.addEventListener('click', () => {
+  borrarTodo();
+});
+
+botonC.addEventListener('click', () => {
+  borrar();
+});
+
+botonIgual.addEventListener('click', () => {
+  calcular();
+});
